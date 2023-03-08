b1581f0b70099e69ba379166562badfdb59c4298
TheCongaGuy <drew.evensen@gmail.com>
2023-03-08 09:31:42 -0800

    Progress on Simple Calculator

---
+++ Simple-Calculator.s
@@ -0,0 +1,60 @@
+# Initial Start from: Example program to display a string and an integer.
+# Calculates a mathematical operation on some two ingers given by user.
+# -----------------------------------------------------
+# Data Declarations
+.data
+prompt1: .asciiz "Enter the first number\n"
+prompt2: .asciiz "Enter the second number\n"
+prompt3: .asciiz "Enter the opertaion type\n"
+
+resultPrompt: .asciiz "The result is\n"
+# -----------------------------------------------------
+# text/code section
+.text
+.globl main
+.ent main
+main:
+# $s0 will represent number 1
+# $s1 will represent number 2
+# $s2 will catch the user's requested operation
+# $s3 will store the result of the operation
+# -----------------------------------------------------
+la $a0, prompt1 # Load prompt 1 into the argument list for syscall
+
+li $v0, 4       # Load call code for printing a string to the console
+syscall         # Run the operation specified at v0 with arguments in a0
+
+li $v0, 5       # Load call code for reading integer input from user
+syscall
+
+move $s0, $v0   # Move the value taken by user to Register s0
+# -----------------------------------------------------
+la $a0, prompt2 # Load prompt 2 into argument list for syscall
+
+li $v0, 4       # Load call code for printing a string to the console
+syscall         
+
+li $v0, 5       # Load call code for reading integer input from user
+syscall
+
+move $s1, $v0   # Move the value taken by user to Register s1
+# -----------------------------------------------------
+la $a0, prompt3 # Load prompt 3 into argument list for syscall
+
+li $v0, 4       # Load call code for printing a string to the console
+syscall         
+
+li $v0, 5       # Load call code for reading integer input from user
+syscall
+
+move $s2, $v0   # Move the value taken by user to Register s2
+# -----------------------------------------------------
+la $a0, resultPrompt # Load the result prompt into argument list
+li $v0, 4       # Load call code for printing a string to the console
+syscall         # Run the same operation with different argument
+
+# -----
+# Done, terminate program.
+li $v0, 10  # Load call code for terminating a program
+syscall     # Run operation specified at v0
+.end main
